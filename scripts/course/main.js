1d476fe30654b0d74661a7a1599d00db01e39df2
Alexey Korovko <akorovko@wsgc.com>
2024-01-19 11:58:43 -0600

    updated course work

--- scripts/course/main.js
+++ scripts/course/main.js
@@ -1,19 +1,18 @@
-/* eslint-disable no-unused-vars */
 const btnEl = document.getElementById('purple-btn');
 const inputEl = document.getElementById('name');
 
 function submitForm(event) {
-  event.preventDefault();
-  const name = inputEl.value;
-  if (name) {
-    const difficulty = document.querySelector('input[name="difficulty"]:checked');
-    sessionStorage.setItem('USER', name);
-    sessionStorage.setItem('DIFFICULTY', difficulty.value);
-    window.location.href = 'game.html';
-  } else {
-    inputEl.style.backgroundColor = "#f5dadf";
-    inputEl.placeholder = "Введите ваше имя";
-  }
+    event.preventDefault();
+    const name = inputEl.value;
+    if (name) {
+        const difficulty = document.querySelector('input[name="difficulty"]:checked');
+        sessionStorage.setItem('USER', name);
+        sessionStorage.setItem('DIFFICULTY', difficulty.value);
+        window.location.href = 'game.html';
+    } else {
+        inputEl.style.backgroundColor = "#f5dadf";
+        inputEl.placeholder = "Введите ваше имя";
+    }
 }
 
 btnEl.addEventListener('click', submitForm);
